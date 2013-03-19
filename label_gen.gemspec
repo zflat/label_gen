4b8b9f475ef444bc0767b2e0214a517bbc42df47
William Wedler <wwedler@riseup.net>
2013-03-19 03:38:04 -0400

    coordinate traversal for frame iter

--- label_gen.gemspec
+++ label_gen.gemspec
@@ -19,7 +19,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency(%q<rspec>, [">= 1.3.0"])
+  spec.add_development_dependency(%q<rspec>, [">= 2.12.0"])
+  spec.add_development_dependency(%q<rspec-core>, [">= 2.12.0"])
   spec.add_development_dependency "rake"
   spec.add_development_dependency 'sqlite3', '~> 1.3.3'
   
--- lib/label_gen/frame_iter.rb
+++ lib/label_gen/frame_iter.rb
@@ -1,27 +1,16 @@
+require 'matrix'
+
 class FrameIter
   
-  
   def initialize(params)
-    params.merge!{
-      :n_x => 1,
-      :n_y => 1,
-      :origin_x => 0,
-      :origin_y => 0,
-      :delta_x => 0,
-      :delta_y => 0
-      :frame_width => 0
-      :frame_height => 0
-    }
-    @geom = OpenStruct.new(params)
-
     @n_x = params[:n_x] || 1
     @n_y = params[:n_y] || 1
     @origin_x = params[:origin_x] || 0
     @origin_y = params[:origin_y] || 0
-    @delta_x =  params[:delta_x] || 0
-    @delta_y =  params[:delta_x] ||0
-    @frame_width = params[:frame_width] || 0
-    @frame_height = params[:frame_height] || 0
+    @delta_x =  params[:delta_x] || 1
+    @delta_y =  params[:delta_y] ||1
+    @frame_width = params[:frame_width] || 1
+    @frame_height = params[:frame_height] || 1
   end
 
   attr_reader :origin_x, :origin_y, :n_x, :n_y, :delta_x, :delta_y, :frame_width, :frame_height
@@ -29,31 +18,28 @@ class FrameIter
   include Enumerable
   
   def x_col
-    ((0..(n_x-1)).map {|i| i*delta_x + origin_x })
+    ((0..(n_x-1)).map {|i| i*(delta_x+frame_width) + origin_x }) * n_y
   end
 
   def y_col
-    ((0..(n_y-1)).map {|i| i*delta_y + origin_y })
+    ((0..(n_y-1)).map {|i| [i*(delta_y+frame_height) + origin_y]*n_x }).flatten
   end
 
-  def coord(i, j)
-    [i*delta_x+origin_x, j*delta_y+origin_y]
+  def total_height
+    y_col[-1]+delta_y+frame_height - origin_y
   end
-  
+
+  def total_width
+    coordinates[-1,0]+delta_x+frame_width - origin_x
+  end
+
   def coordinates
-    Matrix.build(n_x, n_y) do |i, j|
-      coord(i,j)
-    end
+    Matrix[x_col, y_col].transpose
   end
 
   def each
-    int x=origin_x
-    int y=origin_y
-    
-    (0..n_x).each do |x_n|
-      coord_x = x_n * 
-    self.to_a.each do |s|
-      yield s
+    self.coordinates.row_vectors.each do |r|
+      yield r
     end
   end #each
 
