0ed4cb7bd233d3f1f38434c144c114fb775e09c6
William Wedler <wwedler@riseup.net>
2013-03-20 17:52:45 -0400

    implementing using and confirming number records

--- lib/label_gen/number_record.rb
+++ lib/label_gen/number_record.rb
@@ -3,24 +3,46 @@ module LabelGen
 
     include DataMapper::Resource
 
-    # Get the last confirmed number from the database
+    # Get the last number record from the database
     # or the max number from the configuration,
     # whichever is higher
     def self.max_number_used
-      LabelGen.configuration.max_number_used
+      [LabelGen.configuration.max_number_used,
+       self.max_number_persisted].max
     end
 
+    # Get the last confirmed number from the database
+    # or the max number from the configuration,
+    # whichever is higher
+    def self.max_number_confirmed
+      [LabelGen.configuration.max_number_used,
+       self.max_persisted_confirmed].max
+    end
+    
     # Store the given number as used
+    # unless it is already stored
     def self.put_used(number)
-      unless confirm_used(number)
-        number if self.new(number, :used => true).save
+      unless self.confirmed?(number)
+        number if self.first(:number => number) || self.new(number, :used => true).save
       end
     end
 
-    # Confirm that the number was used
+    # Store a record to confirm that the number was used
     def self.confirm_used(number)
       r = self.first(:number => number)
-      r.confirm(true) unless r.nil?
+      ret = r.confirm!(true) unless r.nil?
+    end
+
+    # Check if the number has been used
+    def self.used?(number)
+      r = self.first(:number => number)
+      r && r.used?
+    end
+
+    # Check if the number has been used and confirmed
+    def self.confirmed?(number)
+      r = self.first(:number => number)
+      r && r.used? && r.confirmed?
     end
 
     property :id, Serial
@@ -28,34 +50,54 @@ module LabelGen
     property :used_at, DateTime
     property :confirmed_at, DateTime
 
-    def initialize(number, flags={})
-      @number = number
+    validates_uniqueness_of :number
 
-      self.use flags[:used]
-      self.confirm flags[:confirm]
+    def initialize(number_val, flags={})
+      self.number = number_val
+
+      self.use! flags[:used]
+      self.confirm! flags[:confirm]
     end
-
+    
     attr_reader :number
 
-    def use(check)
-      used_at = check ? Time.now : nil
+    def use!(check)
+      if check
+        self.used_at = Time.now
+        self.save
+      end
     end
 
-    def confirm(check)
-      if self.used?
-        confirmed_at = check ? Time.now : nil
+    def confirm!(check)
+      if self.used? && check
+        self.confirmed_at = Time.now
+        self.save
       end
     end
 
     # Indicate if the number
     # is in the database
     def used?
+      self.used_at
     end
 
     # Indicate if the number is 
     # confirmed as used
     def confirmed?
-      false
+      self.confirmed_at
+    end
+
+    private
+    
+    # Get the highest number that is stored
+    # or 0 if nothing is stored
+    def self.max_number_persisted
+      self.count > 0 ? self.last.number : 0
+    end
+
+    def self.max_persisted_confirmed
+      max_persisted = self.last(:confirmed_at.not => nil)
+      (max_persisted.nil?) ? 0 : max_persisted.number
     end
 
   end
---
+++ spec/number_record_spec.rb
@@ -0,0 +1,136 @@
+require "spec_helper"
+
+module LabelGen
+  describe NumberRecord do
+    context "with an empty db" do
+      before :each do
+        DataMapper.auto_migrate!
+      end
+      
+      it "has a max number used as specified in the configuration" do
+        expect(NumberRecord.max_number_used).to eq LabelGen.configuration.max_number_used
+      end
+      it "has no records" do
+        expect(NumberRecord.count).to eq 0
+      end
+    end # context "with an empty db"
+
+    describe "using a number" do
+      let(:number){1}
+      before :each do
+        DataMapper.auto_migrate!
+        @n = NumberRecord.put_used(number)
+      end
+      it "affirms the number" do
+        expect(@n).to eq number
+      end
+
+      it "has a record" do
+        expect(NumberRecord.first(:number => number)).to_not be_nil
+        expect(NumberRecord.first(:number => number).number).to eq number
+      end
+      
+      it "is used" do
+        expect(NumberRecord.used?(number)).to be_true
+      end
+      
+      it "makes the table non-empty" do
+        expect(NumberRecord.count).to be_> 0
+      end
+      it "is not max confirmed" do
+        expect(NumberRecord.max_number_confirmed).to_not eq number
+      end
+
+      it "has nil for confirmed_at" do
+        expect(NumberRecord.all(:number => number)).to_not be_nil
+        expect(NumberRecord.first(:number => number).confirmed_at).to be_nil        
+      end
+
+      context "which is larger than the max number" do
+        let(:number_max){NumberRecord.max_number_confirmed + 1}
+
+        before :each do
+          DataMapper.auto_migrate!
+          NumberRecord.put_used(number_max)
+        end
+        
+        it "is the max number used" do
+          expect(NumberRecord.max_number_used).to eq number_max
+        end
+        
+        describe "is confirmed" do
+          let(:confirmed_max){NumberRecord.max_number_confirmed + 1}
+          before :each do
+            DataMapper.auto_migrate!
+            @bln_put = NumberRecord.put_used(confirmed_max)
+            @bln_ret = NumberRecord.confirm_used(confirmed_max) unless confirmed_max.nil?
+          end
+          
+          it "is used" do
+            expect(NumberRecord.first(:number => confirmed_max)).to be_used
+          end
+
+          it "has DateTime for confirmed_at" do
+            expect(NumberRecord.first(:number => confirmed_max).confirmed_at).to_not be_nil        
+          end
+
+          it "has succuess confirming" do
+            expect(@bln_ret).to be_true
+          end
+
+          it "has succuess putting" do
+            expect(@bln_put).to be_true
+          end
+
+          it "can be verified as confirmed" do
+            expect(NumberRecord.max_number_confirmed).to eq confirmed_max
+          end
+        end # describe "is confirmed"
+
+      end # context "which is larger than the max number"
+    end # describe "using a number"
+
+    describe "using a previously used, unconfirmed, number" do
+      let(:number){1}
+      before :each do
+        DataMapper.auto_migrate!
+        @n = NumberRecord.put_used(number)
+      end
+      
+      it "returns the number" do
+        expect(NumberRecord.put_used(number)).to eq number
+      end
+    
+      it "does not add a new record" do
+        expect(NumberRecord.all(:number => number).count).to eq 1
+      end
+
+    end # describe "using a previously used, unconfirmed, number" do
+
+    describe "putting a previously confirmed number" do
+      let(:number){1}
+      before :each do
+        DataMapper.auto_migrate!
+        NumberRecord.put_used(number)
+        NumberRecord.confirm_used(number)
+      end
+
+      it "is returns nil" do
+        expect(NumberRecord.put_used(number)).to be_nil
+      end
+    end # describe "using a previously confirmed number"
+    
+    describe "confirming an unused number" do
+      let(:number){1}
+      before :each do
+        DataMapper.auto_migrate!
+        NumberRecord.create(number)
+      end
+      
+      it "is not successull" do
+        expect(NumberRecord.confirm_used(number)).to_not be_true
+      end
+    end # describe "confirming an unused number"
+    
+  end # describe NumberRecord
+end # module LabelGen
